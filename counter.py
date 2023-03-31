5662a04bdbecceec572f56ad41c67d0b48f72f1b
Chelsea Hamilton <chels@Chelseas-iMac.local>
2023-03-30 20:27:34 -0500

    find max email sender

--- counter.py
+++ counter.py
@@ -1,8 +1,23 @@
 def countEmail():
     # This first line is provided for you
-    name = input("Enter file:")
-    if len(name) < 1 : name = "mbox-short.txt"
-    handle = open(name)
+    file = input('Enter file name:')
+    if len(file)<1: file = 'mbox-short.txt'
+    file = open(file)
+    sender = dict()
+    
+    for line in file:
+        if not line.startswith("From:"): continue
+        line = line.split()
+        name = line[1]
+        if name not in sender:
+            sender[name] = 1
+        else:
+            sender[name] +=1
+    value = sender.values()
+    maxval = max(value)
+    person = max(sender,key = sender.get)
+    print(person,maxval)
+
         
 
 ## if you want to test locally run > python counter.py
